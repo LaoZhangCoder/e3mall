16e17eabf5429aa1f1631852702d02d2983d03f5
laozhang <1044985343@qq.com>
2018-08-28 09:23:05 +0800

    提交工具类

---
+++ e3-common/src/main/java/cn/e3mall/common/pojo/SearchResult.java
@@ -0,0 +1,30 @@
+package cn.e3mall.common.pojo;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class SearchResult implements Serializable {
+	private List<SearchItem> itemList;
+	private int totalPages;
+	private long recourdCount;
+	public List<SearchItem> getItemList() {
+		return itemList;
+	}
+	public void setItemList(List<SearchItem> itemList) {
+		this.itemList = itemList;
+	}
+	public int getTotalPages() {
+		return totalPages;
+	}
+	public void setTotalPages(int totalPages) {
+		this.totalPages = totalPages;
+	}
+	public long getRecourdCount() {
+		return recourdCount;
+	}
+	public void setRecourdCount(long numFound) {
+		this.recourdCount = numFound;
+	}
+
+
+}
